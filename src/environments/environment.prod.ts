21d40f8dc294c6da43efa9649a31d29d6dc0edf1
Vishal3kt <vishal.m@3ktechnologies.com>
2026-03-13 15:02:42 +0530

    changes added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl1: 'https://3ktsandbox.api.crm.dynamics.com/',
+  // apiUrl1: 'https://3ktsandbox.api.crm.dynamics.com/',
+  apiUrl1: 'https://3kt-po-sbx.crm.dynamics.com/',
   enableMockLogin: false
 };
